a36a3f14273ef35048289fda368587e900c97157
Lawrence Tam <Tam_Lawrence@bah.com>
2020-11-20 11:35:32 -0500

    Added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport"); // we don't assign to variable because we just want to execute the code in the file
@@ -9,6 +10,8 @@ require("./services/passport"); // we don't assign to variable because we just w
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -22,6 +25,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // ex. main.js, main.css
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("__dirname", "client", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("listening on port " + PORT));
