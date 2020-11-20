41d1976a39c00c582caa84e5a8be8b96cdbf6d23
Lawrence Tam <Tam_Lawrence@bah.com>
2020-11-20 16:21:46 -0500

    Fixed error in index.js in server directory

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   // Express will serve up index.html if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("__dirname", "client", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
